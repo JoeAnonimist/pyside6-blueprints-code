9a6f9727250211e56da54ab4bc14983984fc3024
Joe <anonimista@outlook.com>
2025-12-07 19:35:55 +0100

    Sync: Python files updated from source repo (2025-12-07 19:35:56)

--- code/02_signals_and_slots/02_using_lambdas.py
+++ code/02_signals_and_slots/02_using_lambdas.py
@@ -1,17 +1,8 @@
-# If the signature of a signal matches 
-# the signature of the receiving slot
-# you can use the signal's arguments in the slot.
-# If a slot has a shorter signature 
-# than the signal it is connected with 
-# the extra arguments are ignored
-
 # Demonstrate using a Python lambda function
 # as a slot. Lambdas are anonymous functions,
 # ie. they have no name.
 
-
 import sys
-
 from PySide6.QtWidgets import (QApplication, 
     QWidget, QPushButton, QVBoxLayout)
 
@@ -21,30 +12,28 @@ class Window(QWidget):
     def __init__(self):
 
         super().__init__()
-        
         layout = QVBoxLayout()
         self.setLayout(layout)
         
-        # 1 - Create the widget, same as in the basic example
+        # 1 - Create the widget
         
         button = QPushButton('Click me!')
-        button.setCheckable(True)
-        
-        # 2 - In this case the slots are Python lambdas
-        #     placed in the button.clicked.connect() call.
-        #     We have two slots connected to the same signal.
 
-        button.clicked.connect(lambda: print('Clicked...'))
-        button.clicked.connect(lambda checked: print(checked, 'Something else'))
-        
+        # 2 - In this case the slot is a Python lambda
+
+        button.clicked.connect(
+            lambda : self.log('My log message'))
+
         layout.addWidget(button)
+    
+    def log(self, message):
+        print('Button clicked')
+        print(message)
         
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-
     main_window = Window()
     main_window.show()
-    
     sys.exit(app.exec())
