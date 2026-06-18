c17d30a23e03974334dbd98db3653a1fa1142a8c
Joe <anonimista@outlook.com>
2026-06-18 18:56:43 +0200

    Sync: Python files updated from source repo (2026-06-18 18:56:43)

--- code/01_getting_started/helloworld.py
+++ code/01_getting_started/helloworld.py
@@ -1,9 +1,5 @@
-# A script that just displays
-# an empty Qt6 window
-
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
-
 
 # 1 - Create a class inherited from QWidget
 
@@ -14,17 +10,14 @@ class Window(QWidget):
         # If you don't init the superclass 
         # you get a run-time error
         super().__init__()
+        self.resize(300, 120)
     
 
 if __name__ == '__main__':
     
     # 2 - Create an instance of the QApplication class
-    #     Make sure there isn't one already running.
-    
-    if not QApplication.instance():
-        app = QApplication(sys.argv)
-    else:
-        app = QApplication.instance()
+
+    app = QApplication(sys.argv)
 
     # 3 - Create an instance of the Window class
     #     and show() it
